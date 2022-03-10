19c789630e2a092cdccfe95d113b0c756de8ff85
leoBG <leobgely@gmail.com>
2022-03-10 18:03:25 +0100

    Editeur nivo début

--- Unity2eSem/Assets/Scripts/Evenements/Editor/EditeurNivoFenetre.cs
+++ Unity2eSem/Assets/Scripts/Evenements/Editor/EditeurNivoFenetre.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace Evenements.Editor
 {
@@ -7,7 +9,20 @@ namespace Evenements.Editor
     {
         private void OnGUI()
         {
-            
+            if (GUILayout.Button("Sauvergarder"))
+            {
+                Sauvegarder();
+            }
+        }
+
+        private static void Sauvegarder()
+        {
+            Object[] assets = AssetDatabase.LoadAllAssetsAtPath("Assets/Narration");
+            foreach (var asset in assets)
+            {
+                EditorUtility.SetDirty(asset);
+            }
+            AssetDatabase.SaveAssets();
         }
     }
 }
